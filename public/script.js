ff5258fb3f4f478cb8ec7691b7c7da6595f88bf4
Ahmed Omer <ahmed2002.kirkuk@gmail.com>
2025-06-25 00:34:08 +0300

    auth-page

--- public/script.js
+++
@@ -1,9 +0,0 @@
-function showLogin(){
-    document.getElementById('login').style.display = 'block';
-    document.getElementById('signup').style.display = 'none';
-};
-
-function showSignup(){ 
-    document.getElementById('signup').style.display = 'block';
-    document.getElementById('login').style.display = 'none';
-}
